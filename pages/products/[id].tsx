2349d51bf179c7047a75308faba428594ba0c983
luiz felipe fornari <luizfelipefornari@gmail.com>
2023-08-22 15:51:19 -0300

    corrigindo bugs

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -7,7 +7,6 @@ import { Container } from "reactstrap";
 import Header from "../../src/components/Header";
 import ProductDetails from "../../src/components/ProductDetails";
 import { fetchProduct, fetchProducts, ProductType } from "../../src/services/Products";
-import React from "react";
 
 export const getStaticProps: GetStaticProps = async (context) => {
   const id = context.params?.id
@@ -21,37 +20,35 @@ export const getStaticProps: GetStaticProps = async (context) => {
   return { redirect: { destination: '/products', permanent: false } }
 }
 
-
 export const getStaticPaths: GetStaticPaths = async () => {
-    const products = await fetchProducts()
-  
-    const paths = products.map(product => {
-      return { params: { id: product.id.toString() } }
-    })
-  
-    return { paths, fallback: false }
-  }
+  const products = await fetchProducts()
 
+  const paths = products.map(product => {
+    return { params: { id: product.id.toString() } }
+  })
+
+  return { paths, fallback: false }
+}
 
 const Product: NextPage = (props: {
-    children?: ReactNode
-    product?: ProductType
-  }) => {
-    return (
-      <div>
-        <Head>
-          <title>{props.product!.name}</title>
-          <meta name="description" content={props.product!.description} />
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-  
-        <Header />
-  
-        <Container className="mt-5">
-          <ProductDetails product={props.product!} />
-        </Container>
-      </div>
-    )
-  }
-  
-  export default Product
+  children?: ReactNode
+  product?: ProductType
+}) => {
+  return (
+    <div>
+      <Head>
+        <title>{props.product!.name}</title>
+        <meta name="description" content={props.product!.description} />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Header />
+
+      <Container className="mt-5">
+        <ProductDetails product={props.product!} />
+      </Container>
+    </div>
+  )
+}
+
+export default Product
